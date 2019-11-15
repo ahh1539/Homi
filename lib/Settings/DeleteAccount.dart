a73fa1c746e6b568cd18b34d0b4a814bf01fc277
pedro <pab3507@rit.edu>
2019-11-15 01:24:31 -0500

    lit

--- lib/Settings/DeleteAccount.dart
+++ lib/Settings/DeleteAccount.dart
@@ -1,3 +1,4 @@
+import 'package:animation_exp/login/LoginPage.dart';
 import 'package:flutter/material.dart';
 
 class DeleteAccount extends StatelessWidget {
@@ -11,17 +12,34 @@ class DeleteAccount extends StatelessWidget {
         padding: const EdgeInsets.all(20),
         children: <Widget>[
           Container(
-            height: 60,
+            height: 140,
             child: const Center(
                 child: Text(
-                  'Delete Account',
-                  style: TextStyle(fontWeight: FontWeight.bold),
+                  'Are you sure you want to delete your profile? This is a permanent action that cannot be undone.',
+                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, fontSize: 20),
                 )),
-            margin: const EdgeInsets.all(5.0),
-            decoration: new BoxDecoration(
-                color: Colors.red,
-                borderRadius: new BorderRadius.all(Radius.circular(20.0))),
+            margin: const EdgeInsets.all(10.0),
+
           ),
+          GestureDetector(
+              onTap: () {
+                Navigator.of(context).push(new PageRouteBuilder(
+                  pageBuilder: (_, __, ___) => new LoginPage(),
+                ));
+              },
+              child: new Container(
+                height: 80,
+                child: const Center(
+                    child: Text(
+                      'Delete Account',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+                    )),
+                margin: const EdgeInsets.all(20.0),
+                decoration: new BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: new BorderRadius.all(Radius.circular(20.0))),
+              )),
+
         ],
       ),
     );
