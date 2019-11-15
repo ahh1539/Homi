5cb0deedf19c4ac03b68e8f1d3da17ec7521a61f
pedro <pab3507@rit.edu>
2019-11-15 01:56:02 -0500

    lit

--- lib/Settings/Notifications.dart
+++ lib/Settings/Notifications.dart
@@ -14,9 +14,8 @@ class Notifications extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(20),
         children: <Widget>[
-          PreferenceTitle('Messaging'),
           PreferencePageLink(
-            'Notifications',
+            'Messaging',
             leading: Icon(Icons.message),
             trailing: Icon(Icons.keyboard_arrow_right),
             page: PreferencePage([
@@ -35,35 +34,27 @@ class Notifications extends StatelessWidget {
               SwitchPreference(
                 'Private Messages from Strangers',
                 'notification_pm_stranger',
-                onEnable: () async {
-                  // Write something in Firestore or send a request
-                  await Future.delayed(Duration(seconds: 1));
-
-                  print('Enabled Notifications for PMs from Strangers!');
-                },
-                onDisable: () async {
-                  // Write something in Firestore or send a request
-                  await Future.delayed(Duration(seconds: 1));
-
-                  // No Connection? No Problem! Just throw an Exception with your custom message...
-                  throw Exception('No Connection');
-
-                  print('Disabled Notifications for PMs from Strangers!');
-                },
               ),
             ]),
           ),
-          Container(
-            height: 60,
-            child: const Center(
-                child: Text(
-                  'Disable Notifications',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                )),
-            margin: const EdgeInsets.all(5.0),
-            decoration: new BoxDecoration(
-                color: Colors.cyan,
-                borderRadius: new BorderRadius.all(Radius.circular(20.0))),
+          PreferencePageLink(
+            'Matching',
+            leading: Icon(Icons.check),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            page: PreferencePage([
+              PreferenceTitle('New Homis'),
+              SwitchPreference(
+                'New Homi Matched',
+                'notification_newpost_friend',
+                defaultVal: true,
+              ),
+              PreferenceTitle('Super Homi'),
+              SwitchPreference(
+                'Super Homi Recommendation',
+                'notification_pm_friend',
+                defaultVal: true,
+              ),
+            ]),
           ),
         ],
       ),
