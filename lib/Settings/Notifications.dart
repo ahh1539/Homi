203ac6cb78231bab3bbf6428ef5e0427e9d51a67
pedro <pab3507@rit.edu>
2019-11-15 01:43:23 -0500

    lit

--- lib/Settings/Notifications.dart
+++ lib/Settings/Notifications.dart
@@ -1,4 +1,8 @@
+import 'package:animation_exp/preferences/preference_title.dart';
 import 'package:flutter/material.dart';
+import 'package:preferences/preference_page.dart';
+import 'package:preferences/preference_page_link.dart';
+import 'package:preferences/switch_preference.dart';
 
 class Notifications extends StatelessWidget {
   @override
@@ -10,6 +14,45 @@ class Notifications extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.all(20),
         children: <Widget>[
+          PreferenceTitle('Messaging'),
+          PreferencePageLink(
+            'Notifications',
+            leading: Icon(Icons.message),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            page: PreferencePage([
+              PreferenceTitle('New Posts'),
+              SwitchPreference(
+                'New Posts from Homis',
+                'notification_newpost_friend',
+                defaultVal: true,
+              ),
+              PreferenceTitle('Private Messages'),
+              SwitchPreference(
+                'Private Messages from Homis',
+                'notification_pm_friend',
+                defaultVal: true,
+              ),
+              SwitchPreference(
+                'Private Messages from Strangers',
+                'notification_pm_stranger',
+                onEnable: () async {
+                  // Write something in Firestore or send a request
+                  await Future.delayed(Duration(seconds: 1));
+
+                  print('Enabled Notifications for PMs from Strangers!');
+                },
+                onDisable: () async {
+                  // Write something in Firestore or send a request
+                  await Future.delayed(Duration(seconds: 1));
+
+                  // No Connection? No Problem! Just throw an Exception with your custom message...
+                  throw Exception('No Connection');
+
+                  print('Disabled Notifications for PMs from Strangers!');
+                },
+              ),
+            ]),
+          ),
           Container(
             height: 60,
             child: const Center(
@@ -27,3 +70,4 @@ class Notifications extends StatelessWidget {
     );
   }
 }
+
